00988e8b3ba00ddab4b9a120a4b6b19a9da4b56e
Administrator@COLVEST1.CO.ZW <Administrator@COLVEST1.CO.ZW>
2019-10-03 16:08:29 +0200

    material-ui

--- src/pages/SignIn/Login.tsx
+++ src/pages/SignIn/Login.tsx
@@ -2,9 +2,12 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem
    IonButton, IonItemDivider, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/react';
 import React from 'react';
 import IonIcon from 'ionicons';
+import styles from './style';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 
 const Login: React.FC = () => {
+
   return (
     <IonPage>
       <IonHeader>
@@ -13,73 +16,29 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-{/*         
-          <IonLabel className="ion-text-center">Sign In</IonLabel>
+          <IonCard>
+              
+                <IonCardHeader>
+                  <IonCardSubtitle>Sign In</IonCardSubtitle>
+                </IonCardHeader>
 
-          
-          <IonItem>
-            <IonInput placeholder="Username"></IonInput>
-          </IonItem>
+                <IonCardContent>
+                <IonItem>
+                  <IonInput placeholder="Username"></IonInput>
+                </IonItem>
 
-         
-         <IonItem>
-           <IonInput placeholder="Password"></IonInput>
-         </IonItem>
+                <IonItem>
+                  <IonInput placeholder="Password"></IonInput>
+                </IonItem>
 
-        
+                <IonButton>Sign In</IonButton>
 
-          <IonButton>Sign In</IonButton> */}
+                </IonCardContent>
 
-<IonCard>
-      <IonCardHeader>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-        <IonCardTitle>Card Title</IonCardTitle>
-      </IonCardHeader>
-
-      <IonCardContent>
-        Keep close to Nature's heart... and break clear away, once in awhile,
-        and climb a mountain or spend a week in the woods. Wash your spirit clean.
-      </IonCardContent>
-    </IonCard>
-
-    <IonCard>
-      <IonItem>
-        <IonIcon name="pin" slot="start" />
-        <IonLabel>ion-item in a card, icon left, button right</IonLabel>
-        <IonButton fill="outline" slot="end">View</IonButton>
-      </IonItem>
-
-      <IonCardContent>
-        This is content, without any paragraph or header tags,
-        within an ion-cardContent element.
-      </IonCardContent>
-    </IonCard>
-
-    <IonCard>
-      <IonItem href="#" class="activated">
-        <IonIcon name="wifi" slot="start" />
-        <IonLabel>Card Link Item 1 .activated</IonLabel>
-      </IonItem>
-
-      <IonItem href="#">
-        <IonIcon name="wine" slot="start" />
-        <IonLabel>Card Link Item 2</IonLabel>
-      </IonItem>
-
-      <IonItem class="activated">
-        <IonIcon name="warning" slot="start" />
-        <IonLabel>Card Button Item 1 .activated</IonLabel>
-      </IonItem>
-
-      <IonItem>
-        <IonIcon name="walk" slot="start" />
-        <IonLabel>Card Button Item 2</IonLabel>
-      </IonItem>
-    </IonCard>
-
+          </IonCard>
       </IonContent>
     </IonPage>
-  );
+    );
 };
 
 export default Login;
