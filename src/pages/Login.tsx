9b43d61cb798cca117e1376f4073ac04cadc5b1e
Administrator@COLVEST1.CO.ZW <Administrator@COLVEST1.CO.ZW>
2019-10-03 14:03:26 +0200

    Login design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,47 +10,19 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        {/*-- Default Input --*/}
-          <IonInput></IonInput>
+        
+          <IonLabel className="ion-text-center">Sign In</IonLabel>
 
-          {/*-- Input with value --*/}
-          <IonInput value="custom"></IonInput>
-
-          {/*-- Input with placeholder --*/}
-          <IonInput placeholder="Enter Input"></IonInput>
-
-          {/*-- Input with clear button when there is a value --*/}
-          <IonInput clearInput value="clear me"></IonInput>
-
-          {/*-- Number type input --*/}
-          <IonInput type="number" value="333"></IonInput>
-
-          {/*-- Disabled input --*/}
-          <IonInput value="Disabled" disabled></IonInput>
-
-          {/*-- Readonly input --*/}
-          <IonInput value="Readonly" readonly></IonInput>
-
-          {/*-- Inputs with labels --*/}
+          
           <IonItem>
-            <IonLabel>Default Label</IonLabel>
-            <IonInput></IonInput>
+            <IonInput placeholder="Username"></IonInput>
           </IonItem>
 
-          <IonItem>
-            <IonLabel position="floating">Floating Label</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
+         
+         <IonItem>
+           <IonInput placeholder="Password"></IonInput>
+         </IonItem>
 
-          <IonItem>
-            <IonLabel position="fixed">Fixed Label</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-
-          <IonItem>
-            <IonLabel position="stacked">Stacked Label</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
       </IonContent>
     </IonPage>
   );
